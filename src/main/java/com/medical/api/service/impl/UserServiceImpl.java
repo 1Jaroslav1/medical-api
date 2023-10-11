f776474da85a3ff6e3ead2c156a2ab2ea332ad5a
Yaroslav <garbar.jarek@gmail.com>
2023-10-11 22:12:15 +0300

    init commit

---
+++ src/main/java/com/medical/api/service/impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.medical.api.service.impl;
+
+import com.medical.api.repository.UserRepository;
+import com.medical.api.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
